e542f426abbb92540c131c65f903430468f83cd5
DomenDomestos <50502028+DomenDomestos@users.noreply.github.com>
2019-05-11 00:05:05 +0300

    task 3

---
+++ OOPZI_task3/OOPZI_task3/WHILE/lib/task2.c
@@ -0,0 +1,34 @@
+//#include <math.h>
+//double summ2(double eps)
+//{
+//	double end = 0;
+//	int u = 0;
+//	for (; fabs(end) <= eps;)
+//	{
+//		end += u;
+//		++u;
+//	}
+//	return end;
+//}
+double summ2(double eps)
+{
+	double end = 0;
+	int u = 0;
+	while (fabs(u) <= eps)
+	{
+		end += pow(-1, u) / ((u + 1)*(u + 2)*(u + 3));
+		++u;
+	}
+	return end;
+}
+//double summ2(double eps)
+//{
+//	double end = 0;
+//	int u = 0;
+//	do
+//	{
+//		end += u;
+//		++u;
+//	} while (fabs(end) <= eps);
+//	return end;
+//}
